5d07325344d95f304be5ad46cfc8d2011969acbb
pnk0307 <pnk0307@seoitv.com>
2022-12-21 16:22:19 +0900

    subtree 안한곳에서 다시 commit

--- child/childCore/Animal.cs
+++ child/childCore/Animal.cs
@@ -8,6 +8,6 @@ namespace childCore
 {
     public class Animal
     {
-        public static void Do() => Console.WriteLine("Bunbo2123132 World");
+        public static void Do() => Console.WriteLine("Bunbo2123112312332 Wor123123ld");
     }
 }
