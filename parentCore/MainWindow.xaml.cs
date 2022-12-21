9255ee95fd329c0b273d64d36432a64e15e43424
pnk0307 <pnk0307@seoitv.com>
2022-12-21 15:24:48 +0900

    * Init/

---
+++ parentCore/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace parentCore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += OnInitLoaded;
+        }
+
+        private void OnInitLoaded(object sender, RoutedEventArgs e_)
+        {
+            this.tDebug.Click += (s, e) => Console.WriteLine("Nallo World");
+            this.Loaded -= OnInitLoaded;
+        }
+    }
+}
