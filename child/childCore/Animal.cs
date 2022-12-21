0e4073a2a6b6d95f8882e58773e4ee827062b1bb
pnk0307 <pnk0307@seoitv.com>
2022-12-21 16:21:30 +0900

    subtree 안 넣은 곳에서 commit push 테스트
    .

--- child/childCore/Animal.cs
+++ child/childCore/Animal.cs
@@ -8,6 +8,6 @@ namespace childCore
 {
     public class Animal
     {
-        public static void Do() => Console.WriteLine("Bunbo2 World");
+        public static void Do() => Console.WriteLine("Bunbo2123132 World");
     }
 }
