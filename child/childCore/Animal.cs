e45911c15d1a9eeff192c875121e016c9ab66fdd
pnk0307 <pnk0307@seoitv.com>
2022-12-21 15:42:30 +0900

    dd
    dd

--- child/childCore/Animal.cs
+++ child/childCore/Animal.cs
@@ -8,6 +8,6 @@ namespace childCore
 {
     public class Animal
     {
-        public static void Do() => Console.WriteLine("Nambo World");
+        public static void Do() => Console.WriteLine("buncha World");
     }
 }
